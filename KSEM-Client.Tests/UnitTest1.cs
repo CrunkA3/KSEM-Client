592de0a91e2a8ab0816469a01de70efcb9d4da3d
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2022-07-28 16:53:32 +0200

    update Tests wit .runsettings file

--- KSEM-Client.Tests/UnitTest1.cs
+++ KSEM-Client.Tests/UnitTest1.cs
@@ -5,13 +5,18 @@ namespace KSEM_Client.Tests
     [TestClass]
     public class UnitTest1
     {
-        KSEMClient? client;
+        private static KSEMClient? client;
+        private static TestContext? testContext;
 
         [AssemblyInitialize]
-        public async Task Init()
+        public static async Task Init(TestContext testContext)
         {
+            UnitTest1.testContext = testContext;
             client = new KSEMClient("http://ksem-76555758");
-            await client.LoginAsync("password");
+
+            var password = testContext.Properties["KsemPassword"]?.ToString();
+            if (string.IsNullOrEmpty(password)) throw new ArgumentNullException(nameof(password), "Please set 'KsemPassword' in your .runsettings file");
+            await client.LoginAsync(password);
         }
 
 
